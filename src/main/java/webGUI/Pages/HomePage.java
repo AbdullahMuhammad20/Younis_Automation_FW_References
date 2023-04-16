555c5cee83b2ac344105e2a3078fd98fd2781a7c
Abdullah Younis <abdmohamed033@gmail.com>
2023-04-16 15:22:38 +0200

    Apply POM architecture and add more sub packages to support API and Web and Mobile and add another Base java class for Base Test with additional one to Base class main(Base Page), and add some Base features in Base Page such as (ClickOn, SendValue, Waiting Function)

--- src/main/java/webGUI/Pages/HomePage.java
+++ src/main/java/webGUI/Pages/HomePage.java
@@ -1,6 +1,32 @@
 package webGUI.Pages;
 
-public class HomePage
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import webGUI.locators.HomeLocators;
+
+public class HomePage extends Base
 {
+
+    public HomePage(WebDriver driver)
+    {
+        super(driver);
+    }
+
+    HomeLocators loc = new HomeLocators();
+
+    public void clickOnRegister()
+    {
+        clickOn(loc.registerLink,10);
+    }
+
+
+    public void clickOnLogin()
+    {
+        clickOn(loc.loginLink,10);
+    }
+
+
+
+
 
 }
---
+++ src/main/java/webGUI/locators/HomeLocators.java
@@ -0,0 +1,20 @@
+package webGUI.locators;
+
+import org.openqa.selenium.By;
+
+public class HomeLocators
+{
+    // All elements will use and interact with it in Home page
+    public final By loginLink = By.className("ico-login");
+
+    public final By registerLink = By.className("ico-register");
+
+    public final By wishlistLink = By.className("ico-wishlist");
+
+    public final By shoppingCartLink = By.className("ico-cart");
+
+    public final By menuHome = By.className("top-menu notmobile");
+
+    public final By mainSlider = By.className("nivo-main-image");
+
+}
